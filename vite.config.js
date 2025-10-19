788e9d9989f0a066148c9a6159dfad1ec3653e6e
mhilmisyarif <hilmisyarif.dev@gmail.com>
2025-10-19 14:10:51 +0700

    add tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // <-- Import Tailwind
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  // Register the Tailwind plugin
+  plugins: [react(), tailwindcss()],
 })
